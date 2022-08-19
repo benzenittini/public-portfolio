8c9088fe20b04d76dd5caa937d332aeb16368c72
Ben <ben.zenittini@gmail.com>
2022-08-19 15:16:21 -0400

    Added final reference. Updated 'read more' capability. Minor tweaks.

--- copyDist.sh
+++ copyDist.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Remember to run 'yarn generate' from portfolio project prior to running this script!
+
 rm -rf $(ls -A | grep -v .git | grep -v CNAME | grep -v copyDist.sh)
 cp -r ../portfolio/dist/. ./
